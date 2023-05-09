1973b4a80763c7f1e9629528d45e292747211506
Pipo <yoris159@gmail.com>
2023-05-09 22:21:35 +0200

    First commit
    First launch of the plugin and…
    It starts correctly!

---
+++ src/cubixmc/Hub.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace cubixmc;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Hub extends PluginBase implements Listener {
+
+    public function onEnable(): void
+    {
+
+    }
+
+}
